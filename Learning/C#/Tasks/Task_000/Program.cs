4818b83197b32688e66594bb5d053bb25458560b
Gavrikov Ivan <gipard6@gmail.com>
2022-02-05 11:38:24 +0500

    Добавил папку Tasks

---
+++ Learning/C#/Tasks/Task_000/Program.cs
@@ -0,0 +1,10 @@
+﻿// Вывести квадрат числа
+
+// Выводим на консоль сообщение
+Console.WriteLine("Введите число->");
+
+string? numberStr = Console.ReadLine();
+
+int number = int.Parse(numberStr);
+
+Console.WriteLine($"Квадрат числа: {number * number}");
